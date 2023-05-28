7a950f05528363ebe5e4d019ca1a578a65f85fc9
anaungurean <anamariaungurean01@gmail.com>
2023-05-28 16:03:38 +0300

    -add-login-page

--- my-app/src/Pages/LoginPage/LoginPage.tsx
+++ my-app/src/Pages/LoginPage/LoginPage.tsx
@@ -1,12 +1,88 @@
-import React from 'react';
+import React, { useState } from 'react';
+import styles from './LoginPage.module.css';
+import { useNavigate } from 'react-router-dom';
+import leftSectionImage from './JavaLearningZone.png';
 
-const LoginPage = () => {
+const Header: React.FC<{}> = () => {
   return (
-    <div>
-      <h1>Login Page</h1>
-      {/* Add your login form elements here */}
-    </div>
+    <nav className={styles.header}>
+      <div className={styles.headerText}>Welcome to JavaLearnZone! Start your Java learning adventure now!</div>
+    </nav>
   );
 };
 
-export default LoginPage;
+const LoginPage: React.FC<{}> = () => {
+  const navigate = useNavigate();
+  const [errorMessage, setErrorMessage] = useState<string>('');
+
+  const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+
+    const username = (document.getElementById('email') as HTMLInputElement).value;
+    const password = (document.getElementById('password') as HTMLInputElement).value;
+    const url = `http://localhost:8085/users/login?email=${username}&password=${password}`;
+
+    try {
+      const response = await fetch(url, {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({ username, password }),
+      });
+
+      if (response.ok) {
+           console.log('Succesful lo');
+        
+      } else {
+         setErrorMessage('Login failed. Please try again.');
+      }
+    } catch (error) {
+      console.error('An error occurred:', error);
+    }
+  };
+
+  return (
+    <>
+      <div className={styles['main']}>
+        <div className={styles['left--section']}>
+          <img src={leftSectionImage} alt="" className={styles['image']} />
+        </div>
+        <div className={styles['right--section']}>
+          <div className={styles['form']}>
+            <p className={styles['heading']}>LOG IN</p>
+            <form className={styles['input-field']} onSubmit={handleSubmit}>
+              <div className={styles['input-field']}>
+                <input type="text" className="input" id="email" name="emailFaculty" placeholder="Email Address" />
+              </div>
+              <div className={styles['input-field']}>
+                <input type="password" className="input" id="password" name="password" placeholder="Password" />
+              </div>
+               {errorMessage && <p className={styles['error-message']}>{errorMessage}</p>}
+              <a className={styles['forgot-password']} href="/inregistrare">
+                Forgot password?
+              </a>
+              <button type="submit">Submit</button>
+              <p className={styles['sign-up']}>
+                Don't have an account? <a href="SignUp.html">Sign Up</a>
+              </p>
+            </form>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+};
+
+const Login: React.FC<{}> = () => {
+  return (
+    <>
+      <Header />
+      <body className={styles['Body']}>
+        <LoginPage />
+      </body>
+    </>
+  );
+};
+
+export default Login;
