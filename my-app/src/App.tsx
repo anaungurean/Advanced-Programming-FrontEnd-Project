756522c000975f78b15d462d1926db5a9534bc3c
anaungurean <anamariaungurean01@gmail.com>
2023-05-28 12:41:20 +0300

    -add-changes

--- my-app/src/App.tsx
+++ my-app/src/App.tsx
@@ -1,26 +1,18 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import LoginPage from './Pages/LoginPage/LoginPage';
+ 
 
-function App() {
+const App: React.FC = (): JSX.Element => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Routes>
+        <Route path="/" element={<LoginPage />} />
+         {/* Add more routes for other pages */}
+      </Routes>
+    </Router>
   );
-}
+};
+
 
 export default App;
---
+++ my-app/src/Pages/LoginPage/LoginPage.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const LoginPage = () => {
+  return (
+    <div>
+      <h1>Login Page</h1>
+      {/* Add your login form elements here */}
+    </div>
+  );
+};
+
+export default LoginPage;
